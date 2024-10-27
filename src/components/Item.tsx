cabf3d639265d519b23a4be8a5d999d95a7e5ccb
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2024-10-27 15:19:35 -0300

    feat: removendo tarefa

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,13 +1,15 @@
 import { Checked } from "./icons/checked";
 import { Unchecked } from "./icons/unchecked";
+import { Trash } from "./icons/trash";
 
 
+export function Item(props: { text: string, completed: boolean, onClick: () => void, removerItem: () => void }) {
 
-export function Item(props: { text: string, completed: boolean, onClick: () => void }) {
     return (
-        <div
+        <div className="w-full flex">
+            <div
             onClick={props.onClick}
-            className="flex flex-row min-h-[50px] w-full items-center cursor-pointer hover:bg-slate-600 bg-slate-800 mb-1 rounded-lg p-3"
+            className="flex flex-row min-h-[50px] w-5/6 items-center cursor-pointer hover:bg-zinc-600 bg-zinc-700 mb-1 rounded-l-lg p-3"
         >
             {props.completed ? (
                 <Checked className=" text-white text-3xl mr-3" />
@@ -15,6 +17,12 @@ export function Item(props: { text: string, completed: boolean, onClick: () => v
                 <Unchecked className=" text-white text-3xl mr-3" />
             )}
             <label className="text-white text-xl">{props.text}</label>
+            </div>
+            <div onClick={props.removerItem} className="w-1/6 flex items-center justify-center bg-red-400 hover:bg-red-600 min-h-[50px] mb-1 rounded-r-lg cursor-pointer text-red-700 hover:text-white">
+                <Trash />
+            </div>
+
         </div>
+        
     )
 }
